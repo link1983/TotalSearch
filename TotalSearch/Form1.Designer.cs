2451fdd5a86956a73baebd1aebe06519ddfb0ef7
xasa <xasa@HURRICANE430S>
2017-06-04 22:46:02 +0800

    项目开始

--- TotalSearch/Form1.Designer.cs
+++ TotalSearch/Form1.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(717, 320);
+            this.Name = "Form1";
+            this.Text = "TotalSearch";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
