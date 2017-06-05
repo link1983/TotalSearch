1211ec22f1590e8c588fa9ecd99c36f39ceeb81f
xasa <xasa@HURRICANE430S>
2017-06-05 22:57:51 +0800

    13

--- TotalSearch/Utilities/DirTools.cs
+++ TotalSearch/Utilities/DirTools.cs
@@ -3,75 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace TotalSearch.Utilities
 {
     public class DirTools
     {
-        List<string> FilesList = new List<string>();
-        List<string> DirectoriesList;
-
-        public List<string> GetAllDirectories(string path)
+        public static List<string> GetAllDirectories(string path)
         {
-            DirectoriesList = new List<string>();
+            List<string> DirectoriesList = new List<string>();
             GetAllDirectories(path, ref DirectoriesList);
             return DirectoriesList;
         }
 
-        public List<string> GetAllFiles(string path)
+        public static List<string> GetAllFiles(string path)
         {
+            List<string> FilesList = new List<string>();
+
             List<string> DirectoriesList = GetAllDirectories(path);
-
-            int taskNums = 1;
-            if (DirectoriesList.Count > 20)
-                taskNums = 1;
-            Action a = new Action(GetFiles_Action);
-            Task[] t = new Task[taskNums];
-            for (int i = 0; i < taskNums; i++)
+            foreach(var d in DirectoriesList)
             {
-                t[i] = new Task(a);
-                t[i].Start();
-            }
-            Task.WaitAll(t);
-            return FilesList;
-        }
-
-        void GetFiles_Action()
-        {
-            DirectoryInfo di;
-            //while (DirectoriesList.Count >100)
-            //{
-            //    di = new DirectoryInfo(DirectoriesList[0]);
-            //    DirectoriesList.RemoveAt(0); //立刻删除头元素，否则会被其它线程获取到同一元素，没有锁，有隐患。
-            //    foreach (var f in di.GetFiles())
-            //    {
-            //        FilesList.Add(f.FullName);
-            //    }
-
-            //}
-            while (true)
-            {
-                lock (DirectoriesList)
-                {
-                    if (DirectoriesList.Count != 0)
-                    {
-                        di = new DirectoryInfo(DirectoriesList[0]);
-                        DirectoriesList.RemoveAt(0); //立刻删除头元素，否则会被其它线程获取到同一元素，没有锁，有隐患。
-                    }
-                    else
-                        break;
-                }
+                DirectoryInfo di = new DirectoryInfo(d);
                 foreach (var f in di.GetFiles())
                 {
                     FilesList.Add(f.FullName);
                 }
-                    
             }
-            
+            return FilesList;
         }
 
-        void GetAllDirectories(string path, ref List<string> DirectoriesList)
+        static void GetAllDirectories(string path, ref List<string> DirectoriesList)
         {
             DirectoryInfo di = new DirectoryInfo(path);
             if (di.GetDirectories().Length == 0)
